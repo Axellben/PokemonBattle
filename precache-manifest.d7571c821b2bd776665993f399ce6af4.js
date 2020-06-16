ce7af7e4e0b151f9fee2ef0a76e828932cbdefa5
Axellben <cretu.alexandru2000@gmail.com>
2020-06-16 14:31:09 +0300

    Updates

---
+++ precache-manifest.d7571c821b2bd776665993f399ce6af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fef242765d90d9633cc1c4ac5fa84a",
+    "url": "/PokemonBattle/index.html"
+  },
+  {
+    "revision": "a7e0afca03dd233cdd77",
+    "url": "/PokemonBattle/static/css/main.a9a11b92.chunk.css"
+  },
+  {
+    "revision": "679d68e68c25d505b8c4",
+    "url": "/PokemonBattle/static/js/2.58f1ceba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokemonBattle/static/js/2.58f1ceba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e0afca03dd233cdd77",
+    "url": "/PokemonBattle/static/js/main.626cc2e6.chunk.js"
+  },
+  {
+    "revision": "a092404e286c4b956a78",
+    "url": "/PokemonBattle/static/js/runtime-main.48fd879f.js"
+  }
+]);
